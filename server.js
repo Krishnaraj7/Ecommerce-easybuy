361744c1dc88670b57854a9a378f61464fc8f7a8
Krishnaraj.B <krishnarajb0942@gmail.com>
2024-07-15 23:26:30 +0530

    new

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const __dirname = path.dirname(__filename)
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,"./client/build")))
+app.use(express.static(path.join(__dirname,"../client/build")))
 
 
 //routees
@@ -41,7 +41,7 @@ app.use('/api/v1/product',productRoute)
 
 //rest api
 app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+    res.sendFile(path.join(__dirname,"../client/build/index.html"))
 })
 
 const port = process.env.PORT || 8080 
